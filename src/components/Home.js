a862a28c64e854ec0764f50d3d6742716274a7a5
sunshine95331 <sunshine95331@gmail.com>
2024-07-19 09:22:32 -0400

    complete/Prodan Markovic

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
           <Col className="my-auto">
             <div>
               <h5 className="color-custom-grey font-weight-bold mb-0">Hey !</h5>
-              <h1 className="color-name-blue text-boldest">I'm Nesa William.</h1>
+              <h1 className="color-name-blue text-boldest">I'm Prodan Markovic.</h1>
               <span className="color-custom-grey h4">
                 <Typist className="TypistExample-message" cursor={{ show: false }}>
                   <span>I'm a </span><span className="font-weight-bold">
@@ -28,7 +28,7 @@ const Home = () => {
               <Button variant="" className="color-custom-blue p-0 mr-3 border-0" aria-label="github" onClick={() => window.open('https://github.com/sunshine95331', '_blank')}><FontAwesomeIcon size="2x" icon={faGithubSquare} /></Button>
               <Button variant="" className="color-custom-blue p-0 mr-3 border-0" aria-label="linkedin" onClick={() => window.open('https://www.linkedin.com/in/nesa-william-3162ba31a/', '_blank')}><FontAwesomeIcon size="2x" icon={faLinkedin} /></Button>
             </div>
-            <Button className="bg-custom-blue font-weight-bold shadow-sm border-0 resume-btn" onClick={() => window.open('/static/Nesa William.pdf', '_blank')}>View Resume</Button>
+            <Button className="bg-custom-blue font-weight-bold shadow-sm border-0 resume-btn" onClick={() => window.open('/static/Prodan Markovic.pdf', '_blank')}>View Resume</Button>
           </Col>
           <Col className="w-100 my-auto d-none d-md-block">
             <DeveloperSvg />
